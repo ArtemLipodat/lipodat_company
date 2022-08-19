7e2021148450b05f7b9d0f2a13bc8c10912eac76
Artem <a.lipodat@mail.ru>
2022-08-19 11:49:31 +0400

    setts

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 export const Header = () => {
-  return(
-    <header>
-
-    </header>
-  )
+  return (
+      <header>
+        <Link href='/'><a><img src="./logo.svg" alt=""/></a></Link>
+      </header>
+  );
 }
